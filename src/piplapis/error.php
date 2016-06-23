15fa99cf0d9e6a3cf83419bd20f0a1ee2cea5050
Yuri Ritvin <yuri.ritvin@pipl.com>
2016-06-23 16:14:19 +0300

    PHP library expects the warnings list to always show up when there's an error

--- src/piplapis/error.php
+++ src/piplapis/error.php
@@ -8,7 +8,7 @@ class PiplApi_APIError extends Exception
     private $error;
     private $warnings;
     private $http_status_code;
-    
+
     public function __construct($error, $warnings, $http_status_code)
     {
         // Extend Exception::__construct and set two extra attributes - 
@@ -18,23 +18,25 @@ class PiplApi_APIError extends Exception
         $this->warnings = $warnings;
         $this->http_status_code = $http_status_code;
     }
-    
+
     public function is_user_error()
     {
         // A bool that indicates whether the error is on the user's side.
         return in_array($this->http_status_code, range(400, 499));
     }
-    
+
     public function is_pipl_error()
     {
         // A bool that indicates whether the error is on Pipl's side.
         return !$this->is_user_error();
     }
-    
+
     public static function from_array($d)
     {
         // Transform the dict to a error object and return the error.
-        return new self($d['error'], $d['warnings'], $d['@http_status_code']);
+        return new self(!empty($d['error']) ? $d['error'] : "",
+            !empty($d['warnings']) ? $d['warnings'] : "",
+            !empty($d['@http_status_code']) ? $d['@http_status_code'] : 0);
     }
 
     public function to_array()
